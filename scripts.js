ef50df2663094e505cc494720437419a389d75ee
riptodavid <davidmoreno52498@gmail.com>
2024-07-19 17:25:39 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 document.getElementById('blur-container').addEventListener('click', function() {
     var contractAddress = document.getElementById('contract-address');
     var overlay = document.getElementById('overlay');
+    contractAddress.textContent = '0xYourContractAddress';
     contractAddress.style.filter = 'none';
     overlay.style.display = 'none';
 });
