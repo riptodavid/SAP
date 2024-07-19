d9b5f90a16de666e6559d897b4682d2ae44fe421
riptodavid <davidmoreno52498@gmail.com>
2024-07-19 16:57:59 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.getElementById('reveal-button').addEventListener('click', function() {
+    var contractAddress = document.getElementById('contract-address');
+    if (contractAddress.style.display === 'none') {
+        contractAddress.style.display = 'block';
+    } else {
+        contractAddress.style.display = 'none';
+    }
+});
