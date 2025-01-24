effe4feb0a40a43ef18947fd569939a3d75f0bc0
riptodavid <davidmoreno52498@gmail.com>
2025-01-24 01:51:24 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 document.getElementById('blur-container').addEventListener('click', function() {
     var contractAddress = document.getElementById('contract-address');
     var overlay = document.getElementById('overlay');
-    contractAddress.textContent = 'WILL BE UPDATED';
+    contractAddress.textContent = 'WILL BE UPDATED WITH ADDRESS';
     contractAddress.style.filter = 'none';
     overlay.style.display = 'none';
 });
